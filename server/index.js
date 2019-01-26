b73dfa096ce8856dc979cbf59d704d55b76ce3f3
Pietro Carrara <piticarrara@gmail.com>
2019-01-26 17:59:32 -0200

    Add express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const { FileSystem } = require('./filesystem.js');
+const express = require('express');
 
 var fs = new FileSystem();
+var router = express();
 
-fs.treeJson().
-then(console.log);
+const TARGET_PATH = process.argv[2];
 
-return;
+// Returns the file system tree
+router.get('/tree', async (req, res) => {
+    fs.tree(TARGET_PATH)
+    .then((json) => res.json(json))
+    .catch(err => res.status(500).json(err));
+});
+
+router.listen(8000);
